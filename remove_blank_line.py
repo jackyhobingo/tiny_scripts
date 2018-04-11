ec1a177cafa028b47c0623a6f0a0469a57ed04fb
Jacky Ho <jackyhobingo@gmail.com>
2018-04-12 07:42:16 +0800

    Modify remove_blank_line handling except

--- remove_blank_line.py
+++ remove_blank_line.py
@@ -1,10 +1,9 @@
 input_file = "input.txt"
 output_file = "output.txt"
 
-output = open(output_file, "w", encoding="utf-8")
-with open(input_file, encoding="utf-8") as fin:
-	for line in fin.readlines():
-		if line.strip():
-			output.write(line)
-output.close()
+with open(output_file, "w", encoding="utf-8") as fout:
+    with open(input_file, encoding="utf-8") as fin:
+        for line in fin.readlines():
+            if line.strip():
+                output.write(line)
 
